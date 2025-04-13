741185e5762da14bf77075ff59a8a0fac045a4cd
yu <yul1024@outlook.com>
2025-04-13 20:19:38 +0800

    Update df_concater.py

--- mapreduce/df_concater.py
+++ mapreduce/df_concater.py
@@ -19,15 +19,15 @@ class DFConcaterInterface(ABC):
     将一个文件夹下所有的df合并为一个df，并将结果保存到指定路径。
     """
     @abstractmethod
-    def concat_df_by_directory(self, *args, **kwargs) -> pd.DataFrame:
+    def concat_dfs_by_directory(self, *args, **kwargs) -> pd.DataFrame:
         """主要方法，聚合一个文件夹下的df。"""
 
     @abstractmethod
-    def get_a_df(self, df_path: str | Path) -> pd.DataFrame:
+    def read_a_df(self, df_path: str | Path) -> pd.DataFrame:
         """读取一个df的方法。"""
 
     @abstractmethod
-    def save_result(self, df: pd.DataFrame) -> None:
+    def save_a_df(self, df: pd.DataFrame, path_to_save: str | Path) -> None:
         """保存最终df的方法。"""
 
 
@@ -52,45 +52,61 @@ class BaseDFConcater(DFConcaterInterface):
         self.target_dir = Path(target_dir)
         self.path_to_save = Path(path_to_save)
 
-    def run(self):
+    def run(self, **kwargs):
         """
         主要方法。
         """
-        result_df = self.concat_df_by_directory()
-        self.save_result(result_df)
+        result_df = self.concat_dfs_by_directory(self.target_dir)
+        self.save_a_df(result_df, self.path_to_save)
 
-    def concat_df_by_directory(self) -> pd.DataFrame:
+    def concat_dfs_by_directory(
+        self,
+        target_dir: Annotated[str | Path, "需要进行连接的文件夹目录。"],
+    ) -> pd.DataFrame:
         """
         合并一个dir中所有的df。
 
+        Args:
+            target_dir: target_dir: 需要进行连接的文件夹目录。
         Return:
             合并后的df。
         """
-        df_list = self._get_df_list()
+        df_path_list = self.get_df_path_list(target_dir)
+        df_list = self._get_df_list(df_path_list)
         result_df = self.concat_df(df_list)
         return result_df
 
-    def _get_df_list(self) -> list[pd.DataFrame]:
+    def _get_df_list(
+        self,
+        df_path_list: list[Path],
+    ) -> list[pd.DataFrame]:
         """
         获得当前文件夹下所有的df，会使用需要实现的读取df文件的方法。
 
+        Args:
+            df_path_list: 待合并的df路径的列表。默认是类中方法get_df_path_list返回的结果。
         Return:
             list[pd.DataFrame], 一个列表，内容是所有的df。
         """
-        df_path_list = self.get_df_path_list()
         df_list = [
-            self.get_a_df(df_path) for df_path in df_path_list
+            self.read_a_df(df_path) for df_path in df_path_list
         ]
         return df_list
 
-    def get_df_path_list(self) -> list[Path]:
+    def get_df_path_list(
+        self,
+        target_dir: Annotated[str | Path, "需要进行连接的文件夹目录。"]
+    ) -> list[Path]:
         """
         遍历当前的文件夹，获取所有是文件的文件路径。
 
+        Args:
+            target_dir: 需要进行连接的文件夹目录。
         Return:
             list[Path], 一个列表，内容是所有的df的路径。
         """
-        df_path_list = [df_path for df_path in self.target_dir.iterdir() if df_path.is_file()]
+        target_dir = Path(target_dir)  # 冗余构建Path对象，确保可是正常使用Path相关方法。
+        df_path_list = [df_path for df_path in target_dir.iterdir() if df_path.is_file()]
         return df_path_list
     
     def concat_df(self, df_list: list[pd.DataFrame]) -> pd.DataFrame:
@@ -119,10 +135,10 @@ class DefaultDFConcater(BaseDFConcater):
     ):
         super().__init__(target_dir, path_to_save)
 
-    def get_a_df(self, df_path: str | Path) -> pd.DataFrame:
+    def read_a_df(self, df_path: str | Path) -> pd.DataFrame:
         ...
 
-    def save_result(self, df: pd.DataFrame) -> None:
+    def save_a_df(self, df: pd.DataFrame, path_to_save: str | Path) -> None:
         ...
 
 
