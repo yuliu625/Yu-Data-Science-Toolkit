6e19dd38cbf8652db677437d5d2528b0b6395434
yu <yul1024@outlook.com>
2025-07-14 14:50:58 +0800

    Update file_number_checker.py

--- utils/file_checker/file_number_checker.py
+++ utils/file_checker/file_number_checker.py
@@ -2,17 +2,25 @@
 文件数量检查器。
 """
 
+from __future__ import annotations
+
 from pathlib import Path
 
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
 
-def check_file_number(dir_path: str | Path) -> int:
+
+def check_file_number(
+    dir_path: str | Path
+) -> int:
     """
     检测一个文件夹下有多少个文件。
 
     Args:
-        dir_path: 需要检测的文件夹路径。可以是字符串或Path对象。
-    Return:
-        不区分文件或文件夹类型的数量结果。
+        dir_path (Union[str, Path]): 需要检测的文件夹路径。可以是字符串或Path对象。
+
+    Returns:
+        int: 不区分文件或文件夹类型的数量。
     """
     dir_path = Path(dir_path)
     print(len(list(dir_path.iterdir())))
@@ -20,4 +28,6 @@ def check_file_number(dir_path: str | Path) -> int:
 
 
 if __name__ == '__main__':
-    check_file_number(r"D:\document\code\deep_learning\paper\news_agent\dataset_pipeline\original\utils")
+    dir_path_ = r""
+    check_file_number(dir_path_)
+
