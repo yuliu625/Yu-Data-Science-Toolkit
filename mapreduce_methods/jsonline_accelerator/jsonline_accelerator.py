1478ff61c4fecefa2f96b83b12227f6f9cdd6651
yu <yul1024@outlook.com>
2025-07-15 20:04:14 +0800

    Create jsonline_accelerator.py

---
+++ mapreduce_methods/jsonline_accelerator/jsonline_accelerator.py
@@ -0,0 +1,15 @@
+"""
+对于jsonline文件的专门加速方法。
+"""
+
+from __future__ import annotations
+
+import jsonlines
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+
+
+class JsonLineAccelerator:
+    ...
+
