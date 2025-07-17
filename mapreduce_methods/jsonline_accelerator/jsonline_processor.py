696d3ccefafb90010643228933e694ba817e525b
yu <yul1024@outlook.com>
2025-07-17 21:30:18 +0800

    Create jsonline_processor.py

---
+++ mapreduce_methods/jsonline_accelerator/jsonline_processor.py
@@ -0,0 +1,15 @@
+"""
+jsonline文件的处理方法。
+"""
+
+from __future__ import annotations
+
+import jsonlines
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+
+
+class JsonlineProcessor:
+    ...
+
