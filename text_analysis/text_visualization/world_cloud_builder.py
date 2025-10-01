bde9fa859948bf6f68b68954daae249b22ef9208
yu <yul1024@outlook.com>
2025-10-01 19:19:25 +0800

    Create world_cloud_builder.py

---
+++ text_analysis/text_visualization/world_cloud_builder.py
@@ -0,0 +1,16 @@
+"""
+构建词云的方法。
+"""
+
+from __future__ import annotations
+
+from wordcloud import WordCloud
+from pathlib import Path
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+
+
+class WorldCloudBuilder:
+    ...
+
